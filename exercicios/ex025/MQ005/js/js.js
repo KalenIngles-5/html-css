3ee00769e9cf5844679f18edeebefd72b70abfd9
Kalen Ingles <kaleningles5@gmail.com>
2026-07-24 18:33:52 -0300

    novo

---
+++ exercicios/ex025/MQ005/js/js.js
@@ -0,0 +1,13 @@
+
+const itens = document.querySelector('#itens')
+
+
+function clickMenu (){
+    if (itens.style.display == 'block') {
+         itens.style.display = 'none'
+
+    } else{
+        itens.style.display = 'block';
+    }
+        
+}
